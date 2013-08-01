d07c681a211062ef986730eb4eaf5ee81eb42563
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-07-31 19:03:02 -0700

    adds fuzzy matching on the name

--- lib/humpday.rb
+++ lib/humpday.rb
@@ -7,7 +7,7 @@ module Humpday
   SOUNDS = {
     'uhoh'            => 'uhoh',
     'guess_what_day'  => 'guess_what_day_it_is',
-    'guess_what_day2' => 'guess_what_day_it_is2',
+    'guess_what_day_2'=> 'guess_what_day_it_is2',
     'huh'             => 'huh_anybody',
     'cmon'            => 'aw_cmon_i_know_you_can_hear_me',
     'laugh'           => 'laugh',
@@ -15,7 +15,9 @@ module Humpday
     'humpday'         => 'humpday'
   }
 
-  def self.play sound_name
+  def self.play name
+    sound_name = self.fuzzy_find name
+    raise UnknownSoundError if sound_name.nil?    
     sound = SOUNDS.fetch(sound_name) { raise UnknownSoundError }
     audio_file = File.expand_path("../../lib/support/#{sound}.m4a", __FILE__)
     Sounder.play audio_file
@@ -29,16 +31,21 @@ module Humpday
     [ "humpday version #{Humpday::VERSION}",
       "Usage: humpday help (this)",
       "Usage: humpday random (picks a random sound)",
-      "Usage: humpday <sound name>",
+      "Usage: humpday <sound name> (it will fuzzy match the name)",
       "Sounds:",
       "  uhoh",
       "  guess_what_day",
-      "  guess_what_day2",
+      "  guess_what_day_2",
       "  huh",
       "  cmon",
       "  laugh",
       "  whoowhoa",
       "  humpday"
     ].join "\n"
+  end
+  
+  private
+  def self.fuzzy_find name
+    SOUNDS.keys.select { |sn| sn.include? name }.first
   end
 end
